7f85fa4b2fb48ade6e4e00226b4c5cfcc9cf3577
Micah Napier <micah@cammy.com>
2015-03-11 10:09:01 +1100

    Initial Commit

---
+++ TheFeed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFeed
+//
+//  Created by Micah Napier on 11/03/2015.
+//  Copyright (c) 2015 Micah Napier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
